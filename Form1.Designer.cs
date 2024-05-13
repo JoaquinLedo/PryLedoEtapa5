c804886995b32a0beb10dce0f16b8cac26bc3a50
joaquin Ledo <joaquin-ledo@hotmail.com>
2024-05-13 11:34:31 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PryLedoEtapa5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnVehiculosAleatorios = new System.Windows.Forms.Button();
+            this.lblAuto = new System.Windows.Forms.Label();
+            this.lblAvion = new System.Windows.Forms.Label();
+            this.LblBarco = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnVehiculosAleatorios
+            // 
+            this.BtnVehiculosAleatorios.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnVehiculosAleatorios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnVehiculosAleatorios.Location = new System.Drawing.Point(141, 471);
+            this.BtnVehiculosAleatorios.Name = "BtnVehiculosAleatorios";
+            this.BtnVehiculosAleatorios.Size = new System.Drawing.Size(559, 46);
+            this.BtnVehiculosAleatorios.TabIndex = 0;
+            this.BtnVehiculosAleatorios.Text = "CREAR";
+            this.BtnVehiculosAleatorios.UseVisualStyleBackColor = false;
+            this.BtnVehiculosAleatorios.Click += new System.EventHandler(this.BtnVehiculosAleatorios_Click);
+            // 
+            // lblAuto
+            // 
+            this.lblAuto.AutoSize = true;
+            this.lblAuto.Location = new System.Drawing.Point(29, 9);
+            this.lblAuto.Name = "lblAuto";
+            this.lblAuto.Size = new System.Drawing.Size(35, 13);
+            this.lblAuto.TabIndex = 1;
+            this.lblAuto.Text = "label1";
+            this.lblAuto.Visible = false;
+            // 
+            // lblAvion
+            // 
+            this.lblAvion.AutoSize = true;
+            this.lblAvion.Location = new System.Drawing.Point(283, 9);
+            this.lblAvion.Name = "lblAvion";
+            this.lblAvion.Size = new System.Drawing.Size(35, 13);
+            this.lblAvion.TabIndex = 2;
+            this.lblAvion.Text = "label1";
+            this.lblAvion.Visible = false;
+            // 
+            // LblBarco
+            // 
+            this.LblBarco.AutoSize = true;
+            this.LblBarco.Location = new System.Drawing.Point(536, 9);
+            this.LblBarco.Name = "LblBarco";
+            this.LblBarco.Size = new System.Drawing.Size(35, 13);
+            this.LblBarco.TabIndex = 3;
+            this.LblBarco.Text = "label1";
+            this.LblBarco.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 529);
+            this.Controls.Add(this.LblBarco);
+            this.Controls.Add(this.lblAvion);
+            this.Controls.Add(this.lblAuto);
+            this.Controls.Add(this.BtnVehiculosAleatorios);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gneracion de Vehiculos Aleatoriamente";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnVehiculosAleatorios;
+        private System.Windows.Forms.Label lblAuto;
+        private System.Windows.Forms.Label lblAvion;
+        private System.Windows.Forms.Label LblBarco;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryLedoEtapa5
+{
+    public partial class Form1 : Form
+    {
+        // Declarar variables para la posición X e Y de los controles
+        int x;
+        int y;
+        // Declarar variables para el ancho y el alto del formulario
+        int formWidth;
+        int formHeight;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Obtener el ancho y el alto del formulario
+            formWidth = this.ClientSize.Width;
+            formHeight = this.ClientSize.Height;
+        }
+
+        clsVehiculos objAuto;
+        clsVehiculos objAvion;
+        clsVehiculos objBarco;
+
+        bool Auto = false;
+        bool Avion = false;
+        bool Barco = false;
+        Random random = new Random();
+
+        private void BtnVehiculosAleatorios_Click(object sender, EventArgs e)
+        {
+            LblBarco.Visible = true;
+            lblAuto.Visible = true;
+            lblAvion.Visible = true;
+            x = random.Next(formWidth - objAuto.pctAuto.Width);
+            y = random.Next(formHeight - objAuto.pctAuto.Height);
+            // Limpiar los textos de los labels
+            lblAuto.Text = "";
+            lblAvion.Text = "";
+            LblBarco.Text = "";
+
+            // Eliminar los PictureBox existentes si ya están creados
+            if (objAuto.pctAuto != null && objAuto.pctAuto.Parent != null)
+            {
+                Controls.Remove(objAuto.pctAuto);
+                Controls.Remove(lblAuto);
+            }
+            if (objAvion.pctAvion != null && objAvion.pctAvion.Parent != null)
+            {
+                Controls.Remove(objAvion.pctAvion);
+                Controls.Remove(lblAvion);
+            }
+            if (objBarco.pctBarco != null && objBarco.pctBarco.Parent != null)
+            {
+                Controls.Remove(objBarco.pctBarco);
+                Controls.Remove(LblBarco);
+            }
+
+            // Generar coordenadas X e Y aleatorias
+            x = random.Next(this.Width - 100); // Ancho del formulario
+            y = random.Next(this.Height - 200); // Alto del formulario 
+
+            int numeroAleatorio = random.Next(1, 4);
+
+            switch (numeroAleatorio)
+            {
+                case 1:
+                    objAuto.CrearAuto();
+                    objAuto.pctAuto.Location = new Point(x, y);
+                    Controls.Add(objAuto.pctAuto);
+                    lblAuto.Text = objAuto.tipoVehiculo;
+                    lblAuto.AutoSize = true;
+                    lblAuto.Location = new Point(x, y + objAuto.pctAuto.Height);
+                    Controls.Add(lblAuto);
+                    break;
+
+                case 2:
+                    objAvion.CrearAvion();
+                    objAvion.pctAvion.Location = new Point(x, y);
+                    Controls.Add(objAvion.pctAvion);
+                    lblAvion.Text = objAvion.tipoVehiculo;
+                    lblAvion.AutoSize = true;
+                    lblAvion.Location = new Point(x, y + objAvion.pctAvion.Height);
+                    Controls.Add(lblAvion);
+                    break;
+
+                case 3:
+                    objBarco.CrearBarco();
+                    objBarco.pctBarco.Location = new Point(x, y);
+                    Controls.Add(objBarco.pctBarco);
+                    LblBarco.Text = objBarco.tipoVehiculo;
+                    LblBarco.AutoSize = true;
+                    LblBarco.Location = new Point(x, y + objBarco.pctBarco.Height);
+                    Controls.Add(LblBarco);
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objAuto = new clsVehiculos();
+            objAvion = new clsVehiculos();
+            objBarco = new clsVehiculos();
+
+            objAuto.CrearAuto();
+            objAvion.CrearAvion();
+            objBarco.CrearBarco();
+        }
+    }
+}
